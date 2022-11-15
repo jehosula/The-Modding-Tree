05f335eb3a15fe548f76aa3918e310c6a691ea3c
jehosula <118267162+jehosula@users.noreply.github.com>
2022-11-15 13:46:45 +0300

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The mechanic Tree",
+	id: "rnfjfjhteaiorsuth",
+	author: "jehosula",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
